ae5f3d51955731f6a46703ac8ac600bcf9215c36
matias25pinto <matias25pinto@gmail.com>
2021-05-25 13:54:17 -0400

    ADD(public):Se agrego el front-end de la página

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const cors = require("cors");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = 3000;
+    this.middleware();
+    this.router();
+  }
+
+  middleware() {
+    //CORS, es un middleware para gestionar el acceso a la api los navegadores lo piden de manera obligatoria
+    this.app.use(cors());
+
+    //JSON, es un middleware que lee y luego parsea lo enviado en el body a un objeto json
+    this.app.use(express.json());
+
+    //Es un middleware que permite hacer publica una carpeta
+    this.app.use(express.static("public"));
+  }
+
+  router() {}
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("App corriendo en el puerto: ", this.port);
+    });
+  }
+}
+
+module.exports = Server;
